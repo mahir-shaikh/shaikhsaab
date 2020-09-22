eca1dfdd338ff3d11d64fadd31af50ddc3f29dc7
mahir-shaikh <mahirthebest95@gmail.com>
2020-09-22 22:22:27 +0530

    Updated URLs in enviroment for API

--- src/app/viewsModule/email/email.service.ts
+++ src/app/viewsModule/email/email.service.ts
@@ -3,14 +3,15 @@ import {HttpClient, HttpHeaders}           from '@angular/common/http';
 // import {Headers, RequestOptions}  from '@angular/http';
 import {Observable}               from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class EmailService {
   constructor (private _http: HttpClient) {}
  
-  private _contactUrl = './assets/data/email.php';
+  serverURL = environment.serverURL;
  
   postEmail(newMail: any): Promise<any>{
-    return this._http.post("http://localhost:9999/sendmail", newMail).toPromise();
+    return this._http.post(this.serverURL +"/sendmail", newMail).toPromise();
   }
 }
